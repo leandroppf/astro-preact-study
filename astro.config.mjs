0c3e20a14f5d340c51249d0fbaf1f50dc975e379
Leandro Pedroso <leandroppf@gmail.com>
2023-11-24 16:36:27 -0300

    feat: added server output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable Preact to support Preact JSX components.
-	integrations: [preact()],
+  // Enable Preact to support Preact JSX components.
+  integrations: [preact()],
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
